99896fe724ba1be0db6d60297a4c306e2daf6a67
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2025-10-27 10:14:59 +0000

    ran gradle

--- src/test/java/game/testFileGenerationCode.java
+++ src/test/java/game/testFileGenerationCode.java
@@ -14,7 +14,7 @@ public class testFileGenerationCode {
         for (int i = 0; i < max; i++) {
             int r1 = r.nextInt(1, 50); // so always greater than zero
             String str = String.valueOf(r1) + "\n";
-            CardGame.saveToTxt("test/testFiles/valid3.txt", str);
+            CardGame.saveToTxt("test/java/testFiles/valid3.txt", str);
         }
     }
 }
