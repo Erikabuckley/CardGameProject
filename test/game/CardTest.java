5a9a2bfacf7200945e2078937f6b4bfa20796f9a
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2025-10-27 09:30:42 +0000

    fixed card

--- test/game/CardTest.java
+++ test/game/CardTest.java
@@ -7,28 +7,24 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
+import game.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class CardTest {
+
+    private Card card;
     //before
     @Before 
     public void setUp() throws Exception {
-        card = new Card();
+        card = new Card(9);
     }
 
     //test card created and value correct
     @Test 
     public void testValue() {
         assertEquals("Test card creation works", 1, card.getValue());
-    }
-
-    //after
-    @After 
-    public void cleanUp() {
-        card.reset();
     }
 }
